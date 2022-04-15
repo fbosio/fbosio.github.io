e2e85995edc0dc5dbd05bf5743cfe44d0cb28e7c
fbosio <fedebosio93@gmail.com>
2022-04-14 21:01:37 -0300

    Remove age and add physics frontend app

--- age.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("age").innerText = getAge();
-
-function getAge() {
-  const now = new Date();
-  const dayBirth = new Date(1993, 12, 28);
-  const elapsed = now - dayBirth;
-  return new Date(elapsed).getUTCFullYear() - 1970;
-}
